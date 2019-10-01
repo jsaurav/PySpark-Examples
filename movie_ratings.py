ef2e96e9d3fa8a2e5685caa3905f19c3c3777167
Saurab <saurajain@deloitte.com>
2019-09-30 22:08:53 -0400

    Added python script for movie ratings from Udemy Spark Course.

---
+++ movie_ratings.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col,lit,sum
+
+with SparkSession \
+    .builder \
+    .master('local[2]') \
+    .config('spark.sql.shuffle.partitions', 25) \
+    .getOrCreate() as spark:
+
+    ratingsSourceDF = spark.read.format('csv') \
+                                .option('inferSchema', 'true') \
+                                .option('header', 'true') \
+                                .load('C:\\saurav\\self\\learning\\spark\\udemy\\movie-data\\ml-20m\\ratings.csv')
+
+    ratingsExtraColumnDF = ratingsSourceDF.withColumn('count', lit(1))
+
+    ratingsGroupedDF = ratingsExtraColumnDF.groupBy(col('rating')).agg(sum(col('count'))).sort(col('rating'))
+
+    ratingsGroupedDF.show()
+
+    print(spark)
